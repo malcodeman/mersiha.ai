f5c7cba07b15834023f7d873b30697ce132137d9
malcodeman <26983729+malcodeman@users.noreply.github.com>
2021-02-28 10:10:33 +0100

    Adds next.js

---
+++ pages/index.js
@@ -0,0 +1,15 @@
+import Head from "next/head";
+
+function Home() {
+  return (
+    <>
+      <Head>
+        <title>Mersiha Ceranic</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <main>Mersiha Ceranic</main>
+    </>
+  );
+}
+
+export default Home;
