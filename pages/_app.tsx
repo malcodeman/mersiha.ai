87c4fc8e97237eb8be0fa42423d0cbe76bebba3e
malcodeman <26983729+malcodeman@users.noreply.github.com>
2021-12-01 18:43:08 +0100

    Adds fathom

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,6 +3,7 @@ import Head from "next/head";
 import { ThemeProvider } from "styled-components";
 import { useInterval } from "react-use";
 import React from "react";
+import { load } from "fathom-client";
 
 import { THEMES, LIGHT_BLUE } from "../styles/themes";
 import Global from "../styles/Global";
@@ -12,9 +13,17 @@ function getRandomNumber(max: number) {
 }
 
 const DELAY = 10 * 1000;
+const FATHOM_SITE_ID = "BZDQICES";
 
 function App({ Component, pageProps }: AppProps) {
   const [theme, setTheme] = React.useState(LIGHT_BLUE);
+
+  React.useEffect(() => {
+    load(FATHOM_SITE_ID, {
+      url: "https://warbler.resumebuilder.dev/script.js",
+      includedDomains: ["mersihaceranic.com", "www.mersihaceranic.com"],
+    });
+  }, []);
 
   useInterval(() => {
     const random = getRandomNumber(THEMES.length);
