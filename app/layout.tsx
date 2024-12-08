8272e053dbb847e0f48f808dd5efde9993d36428
malcodeman <26983729+malcodeman@users.noreply.github.com>
2024-12-08 11:04:23 +0100

    feat: adds google font

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,3 +1,4 @@
+import { B612 } from "next/font/google";
 import { Analytics } from "@vercel/analytics/react";
 import { METADATA } from "./lib/constants";
 import "./globals.css";
@@ -5,6 +6,11 @@ import "./globals.css";
 export const metadata = {
   ...METADATA,
 };
+
+const b612 = B612({
+  subsets: ["latin"],
+  weight: "400",
+});
 
 type Props = {
   children: React.ReactNode;
@@ -15,7 +21,7 @@ export default function RootLayout(props: Props) {
 
   return (
     <html lang="en">
-      <body className="bg-[#0a0a0a] text-[#F2F2F2]">
+      <body className={`bg-[#0a0a0a] text-[#F2F2F2] ${b612.className}`}>
         {children}
         <Analytics />
       </body>
