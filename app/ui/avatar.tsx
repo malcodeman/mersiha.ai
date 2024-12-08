acfc7ebbe45cfd46e05a15f74479bfb616b30c33
malcodeman <26983729+malcodeman@users.noreply.github.com>
2024-12-08 10:39:41 +0100

    feat: improves avatar component

--- app/ui/avatar.tsx
+++ app/ui/avatar.tsx
@@ -1,18 +1,31 @@
 import { Avatar as ArkAvatar, AvatarImageProps } from "@ark-ui/react";
+import { equals } from "ramda";
 import { cn } from "@/lib/utils";
 
 type Props = {
   size?: "xs" | "sm" | "md" | "lg" | "xl" | "2xl";
+  shape?: "square" | "rounded" | "full";
 } & AvatarImageProps;
 
 export function Avatar(props: Props) {
-  const { className, ...rest } = props;
+  const { size = "md", shape = "full", className, ...rest } = props;
 
   return (
     <ArkAvatar.Root>
       <ArkAvatar.Image
         {...rest}
-        className={cn("size-16 rounded-full object-cover", className)}
+        className={cn(
+          "object-cover",
+          equals(size, "xs") && "size-8",
+          equals(size, "sm") && "size-9",
+          equals(size, "md") && "size-10",
+          equals(size, "lg") && "size-11",
+          equals(size, "xl") && "size-12",
+          equals(size, "2xl") && "size-16",
+          equals(shape, "rounded") && "rounded-md",
+          equals(shape, "full") && "rounded-full",
+          className,
+        )}
       />
     </ArkAvatar.Root>
   );
