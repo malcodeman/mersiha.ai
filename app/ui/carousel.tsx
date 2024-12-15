4cb4c43b5b35f2349dd62296b2af534566b37f92
malcodeman <26983729+malcodeman@users.noreply.github.com>
2024-12-15 22:46:24 +0100

    removes indicators

--- app/ui/carousel.tsx
+++ app/ui/carousel.tsx
@@ -1,10 +1,9 @@
 "use client";
 import { useState } from "react";
 import { Carousel as ArkCarousel } from "@ark-ui/react";
-import { dec, equals, ifElse, inc, length, map } from "ramda";
+import { dec, ifElse, inc, length, map } from "ramda";
 import { LuChevronLeft, LuChevronRight } from "react-icons/lu";
 import { useIntervalEffect } from "@react-hookz/web";
-import { cn } from "@/lib/utils";
 
 type Props = {
   items: {
@@ -57,23 +56,10 @@ export function Carousel(props: Props) {
             items,
           )}
         </ArkCarousel.ItemGroup>
-        <ArkCarousel.Control className="absolute bottom-4 left-1/2 flex -translate-x-2/4 items-center gap-2 rounded border border-[#272727] bg-[#161616CC] p-1">
+        <ArkCarousel.Control className="absolute bottom-4 left-1/2 flex -translate-x-2/4 items-center gap-1 rounded border border-[#272727] bg-[#161616CC] p-1">
           <ArkCarousel.PrevTrigger className="disabled:opacity-50">
             <LuChevronLeft />
           </ArkCarousel.PrevTrigger>
-          <ArkCarousel.IndicatorGroup className="flex gap-2">
-            {items.map((item, i) => (
-              <ArkCarousel.Indicator
-                key={item.src}
-                index={i}
-                aria-label="Indicator"
-                className={cn(
-                  "size-3 rounded-full",
-                  equals(i, index) ? "bg-[#00F0FF]" : "bg-[#F2F2F2]",
-                )}
-              />
-            ))}
-          </ArkCarousel.IndicatorGroup>
           <ArkCarousel.NextTrigger className="disabled:opacity-50">
             <LuChevronRight />
           </ArkCarousel.NextTrigger>
